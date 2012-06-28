ad74bfe7643e21a4e8361656be9b402f4af499e1
Anders Carling <anders.carling@d05.se>
2012-06-28 16:43:30 +0200

    Initial implementation of Payload class

--- lib/c2dm_client/payload.rb
+++ lib/c2dm_client/payload.rb
@@ -1,4 +1,20 @@
 module C2dmClient
+  # Payload encapsulates and serializes all info in common between
+  # multiple receivers of the same apns message.
+  #
+  # The same Payload instance can be used for multiple Message
+  # instances, in order to optimize serialization compution time.
   class Payload
+
+    def initialize(collapse_key, data={})
+      @hash = { 'collapse_key' => collapse_key.to_s }
+      data.each_pair { |k,v| @hash['data.%s' % k] = v.to_s }
+      @hash.freeze
+    end
+
+    def to_hash
+      @hash
+    end
+
   end
 end
--- spec/c2dm_client/payload_spec.rb
+++ spec/c2dm_client/payload_spec.rb
@@ -1,4 +1,62 @@
 require 'spec_helper'
 
 describe C2dmClient::Payload do
+
+  context '#initialize' do
+
+    it "should create new object given collapse_key and data array" do
+      payload = C2dmClient::Payload.new('asd', {})
+      payload.should be_a(C2dmClient::Payload)
+    end
+
+    it "should create new object given collapse_key" do
+      payload = C2dmClient::Payload.new('asd')
+      payload.should be_a(C2dmClient::Payload)
+    end
+
+  end
+
+  context '#to_hash' do
+
+    it "should return a hash with collapse_key if no data hash is given" do
+      payload = C2dmClient::Payload.new('asd')
+
+      payload.to_hash.should == {
+        'collapse_key' => 'asd'
+      }
+    end
+
+    it "should return a hash with collapse_key and data hash items" do
+      payload = C2dmClient::Payload.new('asd', {
+        'text' => 'Call me later..',
+        'sound' => 'call.m4a'
+      })
+
+      payload.to_hash.should == {
+        'collapse_key' => 'asd',
+        'data.text'    => 'Call me later..',
+        'data.sound'   => 'call.m4a'
+      }
+    end
+
+    it "should convert all keys & values to strings" do
+      payload = C2dmClient::Payload.new(37, :test => :foo)
+
+      payload.to_hash.should == {
+        'collapse_key' => '37',
+        'data.test'    => 'foo'
+      }
+    end
+
+    it "should return a frozen hash" do
+      payload = C2dmClient::Payload.new('test')
+      payload.to_hash.should be_frozen
+    end
+
+  end
+
+  pending "should validate size of payload"
+
+  pending "should serialize data to HTTP POST data"
+
 end
