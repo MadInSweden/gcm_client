f14b7e2bab1004c07413cf4eeecf983e39695801
Anders Carling <anders.carling@d05.se>
2012-06-28 17:11:46 +0200

    Initial implementation of Dispatcher class

--- lib/c2dm_client/dispatcher.rb
+++ lib/c2dm_client/dispatcher.rb
@@ -1,4 +1,11 @@
 module C2dmClient
   class Dispatcher
+
+    attr_reader :username, :password
+
+    def initialize(username, password)
+      @username, @password = username, password
+    end
+
   end
 end
--- spec/c2dm_client/dispatcher_spec.rb
+++ spec/c2dm_client/dispatcher_spec.rb
@@ -1,4 +1,28 @@
 require 'spec_helper'
 
 describe C2dmClient::Dispatcher do
+
+  context '#initialize' do
+
+    it "should create new object given username and password" do
+      dispatcher = C2dmClient::Dispatcher.new('user', 'password')
+      dispatcher.should be_a(C2dmClient::Dispatcher)
+    end
+
+  end
+
+  pending 'should send single message'
+
+  pending 'should handle initial authentication while sending'
+  pending 'should handle errors when authenticating'
+
+  pending 'should handle http protocol errors while sending'
+  pending 'should handle ssl protocol errors while sending'
+  pending 'should handle network errors while sending'
+  pending 'should handle response error codes while sending'
+  pending 'should handle http 401 (auth) errors while sending'
+  pending 'should handle http 503 (retry later) errors while sending'
+
+  pending 'it should implement nifty logging'
+
 end
